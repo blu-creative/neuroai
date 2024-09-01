8bc61c607dd9d99524cdaadf269b908ea4dc5d02
MohammadK1373 <m.kamalzadeh73@gmail.com>
2024-09-01 20:07:11 +0330

    ok

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -21,29 +21,5 @@ export default function Header() {
         </h1>
       </section>
     </div>
-    // <header className="bg-primary-500 text-neutral-25 hidden md:flex flex-col md:flex-row items-center justify-between px-4 py-4 md:px-20 md:py-8">
-    //   <a
-    //     className="flex gap-2 items-center font-bold text-center md:text-left"
-    //     href="/"
-    //   >
-    //     <div
-    //       className="relative md:w-[42px] md:h-[42px] w-[42px] h-[42px]"
-    //     >
-    //     <Image
-    //       src="/images/Logo.png"
-    //       fill
-    //       quality={100}
-    //       alt="Mitch's Iron & Metal Logo"
-    //     />
-    //     </div>
-
-    //   </a>
-
-    //   <a className="block w-fit" href="#form">
-    //     <button className="bg-primary-700 rounded-md px-6 py-3 font-extrabold text-md">
-    //       {t("get_a_quote")}
-    //     </button>
-    //   </a>
-    // </header>
   );
 }
