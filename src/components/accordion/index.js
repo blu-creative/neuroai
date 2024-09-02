662707275bff05cdcbd962939cc6ede6195a5386
Reza Hashemi <reza.8139@gmail.com>
2024-09-02 15:53:19 +0330

    change icon

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -16,11 +16,13 @@ export default function Accordion({ text, children }) {
         className="text-primary-700 flex items-center gap-1 justify-between cursor-pointer w-full bg-neutral-50 px-6 py-4 rounded-lg"
       >
         <p className="font-extrabold md:text-2xl text-base">{text}</p>
-        <i
-          className={`icon-mim-chevron block text-xl transform transition-transform ${
+        <div
+          className={`text-xl transform transition-transform rounded-full bg-primary-700 w-9 h-9 flex items-center justify-center ${
             height ? "rotate-180" : ""
           }`}
-        />
+        >
+          <i className="icon-mim-chevron block text-neutral-50" />
+        </div>
       </div>
       <div
         style={{ height }}
