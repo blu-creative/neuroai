143254fe4ccf10d4e2ac9950ab350bc48743707b
Reza Hashemi <reza.8139@gmail.com>
2024-09-02 12:31:46 +0330

    change header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,15 @@
 "use client";
 import { useTranslation } from "@/hooks/useTranslation";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, usePathname } from "next/navigation";
 import Links from "@/components/links";
 
-export default function Header() {
+export default function Header({ text }) {
+  const pathname = usePathname();
+  const list = {
+    "/": "Customize your journey",
+    "/accessibility": "Learn about accessibility",
+    "/contact-us": "Let’s address accessibility",
+  };
   const searchParams = useSearchParams();
   const locale = searchParams.get("locale") || "en";
   const { t } = useTranslation(searchParams.get("locale"));
@@ -15,7 +21,8 @@ export default function Header() {
           className="md:text-7xl text-3xl max-w-2xl font-extrabold text-center"
           data-aos="fade-up"
         >
-          {t("customize-your-journey")}
+          {/* {t("customize-your-journey")} */}
+          {list[pathname]}
         </h1>
       </div>
     </header>
