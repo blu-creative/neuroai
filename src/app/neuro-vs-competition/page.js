1323d10650b27ae288daafe55b7f537bebf3d1dd
Reza Hashemi <reza.8139@gmail.com>
2024-09-02 22:14:42 +0330

    change page

--- src/app/neuro-vs-competition/page.js
+++ src/app/neuro-vs-competition/page.js
@@ -1,7 +1,5 @@
 import First from "./components/first";
 import Second from "./components/second";
-import Third from "./components/third";
-import Fourth from "./components/fourth";
 import { useTranslation } from "@/hooks/useTranslation";
 
 export default function AU({ searchParams }) {
