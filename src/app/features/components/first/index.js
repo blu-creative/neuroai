27be5963be5b3cea746b1dcf2fa6237fb21363dd
Reza Hashemi <reza.8139@gmail.com>
2024-09-02 14:31:13 +0330

    change images

--- src/app/features/components/first/index.js
+++ src/app/features/components/first/index.js
@@ -1,66 +1,74 @@
+import Image from "next/image";
 import Link from "next/link";
+
+const textList = [
+  {
+    first: "ADHD Friendly",
+    second: "Profile",
+    description:
+      "Significantly reduces distractions and noise, to help people with ADHD and others to browse, read, and focus on the essential elements of the website more easily.",
+    img: "Feature1.gif",
+  },
+  {
+    first: "Cognitive Disability",
+    second: "Profile",
+    description:
+      "Provides various assistive features to help users with cognitive disabilities such as Autism, Dyslexia, CVA, and others to focus on the essential elements of the website more easily.",
+    img: "Feature2.gif",
+  },
+  {
+    first: "Content",
+    second: "Adjustments",
+    description:
+      "Neuro provides customized content adjustment support, for example, adjusting font size, line height and letter spacing.",
+    img: "Feature3.png",
+  },
+  {
+    first: "Display",
+    second: "Adjustments",
+    description:
+      "Neuro empowers users to adjust the colors and contrast of the site, with customized adjustments like monochrome, background and text colorization and more.",
+    img: "Feature4.gif",
+  },
+  {
+    first: "Orientation",
+    second: "Adjustments",
+    description:
+      "Achieved through links, shortcuts and guiding elements, as well as reducing distractions and noise for individuals with impairments, Neuro permits reorienting the website to suit their needs, like activating the read mode, stopping animations or hiding images.",
+    img: "Feature5.gif",
+  },
+];
 
 export default function First() {
   return (
-    <div className=" bg-cover">
-      <section className="flex flex-col items-center gap-12 py-16 px-8">
-        <div className="flex justify-center px-4 md:px-0">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-[48px] gap-y-[22px] bg-mintcream w-full md:w-[80%] max-w-[80%]">
-            <div
-              data-aos="fade-button"
-              className="bg-neutral-50 flex items-center w-full max-w-[655px] h-[230px] rounded-xl p-4"
+    <div className="flex flex-col items-center gap-12 py-16 px-8 font-CerebriSansPro">
+      {textList.map((text) => (
+        <section className="flex justify-center gap-10 flex-row [&:nth-child(even)]:flex-row-reverse items-center">
+          <div className="flex flex-col gap-7">
+            <h1 className="text-6xl font-bold font-Exo" data-aos="fade-up">
+              <p className="text-primary-500">{text.first}</p>
+              <p className="text-primary-900">{text.second}</p>
+            </h1>
+            <p className="font-bold text-xl max-w-xl" data-aos="fade-down">
+              {text.description}
+            </p>
+            <button
+              data-aos="fade-up"
+              className="bg-primary-500 text-neutral-50 w-fit px-4 py-1 rounded-lg font-medium text-xl"
             >
-              <h2 className="text-2xl font-bold mb-2 text-primary-900">
-                Web accessibility has evolved from a niche consideration to a
-                standard practice driven by legal, ethical, business,
-                technological and institutional factors...
-              </h2>
-            </div>
-            <div
-              data-aos="fade-left"
-              className="bg-neutral-50 flex items-center w-full max-w-[655px] h-[230px] rounded-xl p-4"
-            >
-              <div className="flex-shrink-0 w-[20%] flex justify-center items-start h-[65%] ">
-                <div className="w-[65px] h-[65px] rounded-full flex items-center justify-center bg-primary-500">
-                  <i className="icon-mim-search-page block text-5xl weight300 font-light text-neutral-25" />
-                </div>
-              </div>
-
-              <div className="w-[80%] pl-4">
-                <h2 className="text-2xl mb-2 font-normal text-primary-900">
-                  <span className=" font-bold"> 96%</span> of the{" "}
-                  <span className=" font-bold"> top one million web pages</span>{" "}
-                  had accessibility issues in
-                  <span className=" font-bold"> 2023</span>
-                </h2>
-                <p className="text-sm font-bold">
-                  institute for disability Research, Policy, and practice
-                </p>
-              </div>
-            </div>
+              Learn More
+            </button>
           </div>
-        </div>
-
-        <h1
-          className="md:text-6xl text-3xl max-w-2xl font-extrabold text-center"
-          data-aos="fade-up"
-        >
-          {/* {translate("about_us")} */}
-          <span className="text-primary-800"> What is </span>
-          <span className="text-primary-500">Accessibility?</span>
-        </h1>
-        <h2
-          className="md:text-2xl text-xs max-w-4xl font-bold text-center text-secondary-500"
-          data-aos="fade-down"
-        >
-          {/* {translate("integrating ")} */}
-          In digital contexts, accessibility often refers to making websites,
-          applications, and digital content usable for people with disabilities
-          such as visual, auditory, motor, or cognitive impairments. It is
-          crucial for ensuring that everyone can access and use digital
-          resources without barriers.
-        </h2>
-      </section>
+          <Image
+            data-aos="flip-left"
+            width={500}
+            height={420}
+            quality={100}
+            alt="img"
+            src={`/images/features/${text.img}`}
+          />
+        </section>
+      ))}
     </div>
   );
 }
