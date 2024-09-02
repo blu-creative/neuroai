c1a27d222d21486a168f24402fbdfbd50a1ec579
MohammadK1373 <m.kamalzadeh73@gmail.com>
2024-09-02 17:05:53 +0330

    ok

--- src/app/accessibility/components/third/index.js
+++ src/app/accessibility/components/third/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function First({ translate }) {
   return (
     <div className="w-full flex justify-center my-20 px-2">
-      <div className="flex flex-col md:flex-row justify-center md:justify-around md:gap-40 gap-12 items-center">
+      <div className="flex flex-col md:flex-row justify-center md:justify-around md:gap-30 gap-12 items-center">
         <div className="max-w-xl text-center md:text-left mb-8 md:mb-0">
           {/* <p className="text-neutral-500 text-xl font-bold" data-aos="fade-up">
             {translate("about_us_mission")}
@@ -13,49 +13,105 @@ export default function First({ translate }) {
             data-aos="fade-up"
           >
             {/* {translate("our_objective")} */}
-            How Neuro <span className="text-primary-500"> helps your SEO </span>
+            Disability{" "}
+            <span className="text-primary-900"> and Neurodiversity </span>
           </h1>
           <h2
             className="text-neutral-800 text-base md:text-xl font-bold py-4"
             data-aos="fade-up"
           >
-            {/* {translate("objective_description")} */}
-            Implementing accessibility practices not only makes your website
-            more inclusive but also enhances its SEO performance. Investing in
-            Neuro has a dual benefit of reaching a broader audience and
-            improving your site's search engine visibility and rankings.
+            {/* {translate("objective_description")} */}A disability is any
+            condition of the body or mind that makes it more difficult for the
+            person with the condition to do certain activities and interact with
+            the world around them. For neurodiversity, it describes the idea
+            that there is a wide range of ways that people perceive and respond
+            to the world, and some neurodiverse conditions could fall under the
+            umbrella of disability.
           </h2>
-
-          <ul className="list-disc list-inside marker:text-primary-500 text-xl font-bold">
-            <li data-aos="fade-right" className="mb-2">
-              Improved usability
-            </li>
-            <li data-aos="fade-left" className="mb-2">
-              Mobile friendliness
-            </li>
-            <li data-aos="fade-right" className="mb-2">
-              Enhanced readability
-            </li>
-            <li data-aos="fade-left" className="mb-2">
-              Reduced bounce rates
-            </li>
-            <li data-aos="fade-right" className="mb-2">
-              Faster page loading times
-            </li>
-          </ul>
         </div>
         {/* Image Section */}
         <div
-          className="md:min-w-[400px] md:min-h-[516px] md:w-[410px] md:h-[410px] relative min-w-[330px] min-h-[195px] w-[330px] h-[195px] overflow-hidden rounded-lg"
+          className=" relative min-w-[330px]  overflow-hidden rounded-lg"
           data-aos="flip-left"
         >
-          <Image
-            src="/images/benefits/pic1.png"
-            fill
-            quality={100}
-            alt="Img"
-            className="object-cover object-center"
-          />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-x-[48px] gap-y-[22px] bg-mintcream  ">
+            <div
+              data-aos="fade-button"
+              className=" flex flex-col items-center justify-center w-full max-w-[257px] h-[155px] rounded-xl bg-primary-500 "
+            >
+              <div className="flex justify-between mb-2">
+                <div className="flex-shrink-0 w-[20%] flex justify-center items-start ">
+                  <i className="icon-mim-neurodivergence block text-5xl weight300 font-light text-neutral-25" />
+                </div>
+                <h2 className="text-5xl font-bold text-neutral-25 text-center">
+                  15%
+                </h2>
+              </div>
+
+              <div className="w-[90%]">
+                <p className="text-base font-bold text-center text-neutral-25">
+                  of the world’s population is neurodivergence
+                </p>
+              </div>
+            </div>
+            <div
+              data-aos="fade-button"
+              className=" flex flex-col items-center justify-center w-full max-w-[257px] h-[155px] rounded-xl bg-[#F7F7F7] "
+            >
+              <div className="flex justify-between mb-2">
+                <div className="flex-shrink-0 w-[20%] flex justify-center items-start ">
+                  <i className="icon-mim-neurodivergence block text-5xl weight300 font-light text-primary-700" />
+                </div>
+                <h2 className="text-5xl font-bold text-primary-700 text-center">
+                  6.2M
+                </h2>
+              </div>
+
+              <div className="w-[90%]">
+                <p className="text-base font-bold text-center text-primary-800">
+                  Canadians live with a disability{" "}
+                </p>
+              </div>
+            </div>
+            <div
+              data-aos="fade-button"
+              className=" flex flex-col items-center justify-center w-full max-w-[257px] h-[155px] rounded-xl bg-[#F7F7F7]"
+            >
+              <div className="flex justify-between mb-2">
+                <div className="flex-shrink-0 w-[20%] flex justify-center items-start ">
+                  <i className="icon-mim-neurodivergence block text-5xl weight300 font-light text-primary-700" />
+                </div>
+                <h2 className="text-5xl font-bold text-primary-700 text-center">
+                  1.3B
+                </h2>
+              </div>
+
+              <div className="w-[90%]">
+                <p className="text-base font-bold text-center text-primary-800">
+                  experience significant disability worldwide
+                </p>
+              </div>
+            </div>
+            <div
+              data-aos="fade-button"
+              className=" flex flex-col items-center justify-center w-full max-w-[257px] h-[155px] rounded-xl bg-primary-900 "
+            >
+              <div className="flex justify-between mb-2">
+                <div className="flex-shrink-0 w-[20%] flex justify-center items-start ">
+                  <i className="icon-mim-neurodivergence block text-5xl weight300 font-light text-primary-500" />
+                </div>
+                <h2 className="text-5xl font-bold text-neutral-25 text-center">
+                  13%
+                </h2>
+              </div>
+
+              <div className="w-[90%]">
+                <p className="text-base font-bold text-center text-neutral-25">
+                  Americans are neurodivergent
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
