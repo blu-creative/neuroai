533d7791a2a3b7fb3f8d377534ddbd7f7207c683
MohammadK1373 <m.kamalzadeh73@gmail.com>
2024-10-31 02:28:14 +0330

    ok

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,5 +1,143 @@
+// import Sections from "./sections";
+// const url = process.env.NEXT_PUBLIC_URL;
+
+// export let metadata = {
+//   title: "Default Title",
+//   description: "Default Description",
+//   keywords: "Default Keywords",
+// };
+
+// export default async function Blog({ params, searchParams }) {
+//   const { id } = params;
+//   const locale =
+//     searchParams.locale === "fr"
+//       ? "fr-CA"
+//       : searchParams.locale === "en"
+//       ? "en"
+//       : "";
+
+//   const res = await fetch(
+//     `${url}/api/articles/${id}?locale=${locale}&populate[0]=blocks.file&populate[1]=blocks.files&populate[2]=cover&populate[3]=author&populate[4]=tags&populate[5]=seo`,
+//     {
+//       // Control caching behavior
+//       cache: "no-store", // Fetch fresh data on every request
+//       // next: { revalidate: 60 }, // Revalidate data every 60 seconds
+//     }
+//   );
+
+//   let post = {};
+//   if (res.ok) {
+//     try {
+//       const object = await res.json();
+//       console.log("object:", object);
+
+//       post = object.data;
+//       console.log("post:", post);
+//       if (post.seo && post.seo.length > 0) {
+//         metadata = {
+//           title: post.seo[0].metaTitle || "",
+//           description: post.seo[0].metaDescription || "",
+//           keywords: post.seo[0].keywords || "",
+//         };
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+//   if (post.tags?.length) {
+//     let filter = "";
+//     post.tags.forEach((element, index) => {
+//       filter += `filters[$or][${index}][tags][title][$eq]=${element.title}&`;
+//     });
+
+//     const related = await fetch(
+//       `${url}/api/articles?locale=${locale}&${filter}&populate=cover`,
+//       {
+//         // Control caching behavior
+//         cache: "no-store", // Fetch fresh data on every request
+//         // next: { revalidate: 60 }, // Revalidate data every 60 seconds
+//       }
+//     );
+
+//     if (related.ok) {
+//       try {
+//         const object = await related.json();
+//         post.related = object.data;
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+//   }
+
+//   const nextPost = await fetch(
+//     `${url}/api/articles?filters[publishedAt][$gt]=${post.publishedAt}&sort=publishedAt:asc&pagination[limit]=1`,
+//     {
+//       // Control caching behavior
+//       cache: "no-store", // Fetch fresh data on every request
+//       // next: { revalidate: 60 }, // Revalidate data every 60 seconds
+//     }
+//   );
+
+//   if (nextPost.ok) {
+//     try {
+//       const object = await nextPost.json();
+//       if (object.data.length) {
+//         post.nextId = object.data[0].documentId;
+//       }
+//       post.next = object.data;
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+
+//   return (
+//     <main>
+//       <Sections post={post} locale={locale} />
+//     </main>
+//   );
+// }
+
 import Sections from "./sections";
 const url = process.env.NEXT_PUBLIC_URL;
+
+export async function generateMetadata({ params, searchParams }) {
+  const { id } = params;
+  const locale =
+    searchParams.locale === "fr"
+      ? "fr-CA"
+      : searchParams.locale === "en"
+      ? "en"
+      : "";
+
+  const res = await fetch(
+    `${url}/api/articles/${id}?locale=${locale}&populate[0]=blocks.file&populate[1]=blocks.files&populate[2]=cover&populate[3]=author&populate[4]=tags&populate[5]=seo`,
+    { cache: "no-store" }
+  );
+
+  if (res.ok) {
+    try {
+      const data = await res.json();
+      const post = data.data;
+
+      // Define dynamic metadata based on fetched SEO data or defaults
+      const metadata = {
+        title: post.seo?.[0]?.metaTitle || "Default Title",
+        description: post.seo?.[0]?.metaDescription || "Default Description",
+        keywords: post.seo?.[0]?.keywords || "Default Keywords",
+      };
+      return metadata;
+    } catch (error) {
+      console.error("Error generating metadata:", error);
+    }
+  }
+
+  // Fallback metadata in case of fetch failure
+  return {
+    title: "Default Title",
+    description: "Default Description",
+    keywords: "Default Keywords",
+  };
+}
 
 export default async function Blog({ params, searchParams }) {
   const { id } = params;
@@ -11,12 +149,8 @@ export default async function Blog({ params, searchParams }) {
       : "";
 
   const res = await fetch(
-    `${url}/api/articles/${id}?locale=${locale}&populate[0]=blocks.file&populate[1]=blocks.files&populate[2]=cover&populate[3]=author&populate[4]=tags`,
-    {
-      // Control caching behavior
-      cache: "no-store", // Fetch fresh data on every request
-      // next: { revalidate: 60 }, // Revalidate data every 60 seconds
-    }
+    `${url}/api/articles/${id}?locale=${locale}&populate[0]=blocks.file&populate[1]=blocks.files&populate[2]=cover&populate[3]=author&populate[4]=tags&populate[5]=seo`,
+    { cache: "no-store" }
   );
 
   let post = {};
@@ -28,6 +162,8 @@ export default async function Blog({ params, searchParams }) {
       console.error(error);
     }
   }
+
+  // Fetch related articles based on tags if available
   if (post.tags?.length) {
     let filter = "";
     post.tags.forEach((element, index) => {
@@ -36,11 +172,7 @@ export default async function Blog({ params, searchParams }) {
 
     const related = await fetch(
       `${url}/api/articles?locale=${locale}&${filter}&populate=cover`,
-      {
-        // Control caching behavior
-        cache: "no-store", // Fetch fresh data on every request
-        // next: { revalidate: 60 }, // Revalidate data every 60 seconds
-      }
+      { cache: "no-store" }
     );
 
     if (related.ok) {
@@ -53,13 +185,10 @@ export default async function Blog({ params, searchParams }) {
     }
   }
 
+  // Fetch the next post
   const nextPost = await fetch(
     `${url}/api/articles?filters[publishedAt][$gt]=${post.publishedAt}&sort=publishedAt:asc&pagination[limit]=1`,
-    {
-      // Control caching behavior
-      cache: "no-store", // Fetch fresh data on every request
-      // next: { revalidate: 60 }, // Revalidate data every 60 seconds
-    }
+    { cache: "no-store" }
   );
 
   if (nextPost.ok) {
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,35 +1,84 @@
+import Head from "next/head";
 import First from "./components/first";
 
 export default async function Blog({ searchParams }) {
   const url = process.env.NEXT_PUBLIC_URL;
   const locale = searchParams.locale === "fr" ? "fr-CA" : "en";
 
+  // const res = await fetch(
+  //   `${url}/api/articles?locale=${locale}&populate=cover&pagination[page]=1&pagination[pageSize]=10&sort=publishedAt:desc`,
+  //   {
+  //     // Control caching behavior
+  //     cache: "no-store", // Fetch fresh data on every request
+  //     // next: { revalidate: 60 }, // Revalidate data every 60 seconds
+  //   }
+  // );
+
+  //with from to
+  // const res = await fetch(
+  //   `${url}/api/articles?locale=${locale}&populate[cover]=true&populate=from&populate=to&pagination[page]=1&pagination[pageSize]=10&sort=publishedAt:desc`,
+  //   {
+  //     cache: "no-store",
+  //   }
+  // );
+
   const res = await fetch(
-    `${url}/api/articles?locale=${locale}&populate=cover&pagination[page]=1&pagination[pageSize]=10&sort=publishedAt:desc`,
+    `${url}/api/articles?locale=${locale}&populate=*&pagination[page]=1&pagination[pageSize]=10&sort=publishedAt:desc`,
     {
-      // Control caching behavior
-      cache: "no-store", // Fetch fresh data on every request
-      // next: { revalidate: 60 }, // Revalidate data every 60 seconds
+      cache: "no-store",
     }
   );
 
   let posts = [];
+  let seo = { title: "Default Title", description: "Default Description" }; // Default SEO values
+
   if (res.ok) {
     try {
-      const object = await res.json();
-      posts = object.data;
+      const articles = await res.json();
+      const currentDate = new Date();
+
+      articles.data.forEach((article) => {
+        let fromDate = new Date(article.from);
+        let toDate = article.to ? new Date(article.to) : null;
+
+        // Check if currentDate is between fromDate and toDate (inclusive)
+        const isWithinRange =
+          fromDate <= currentDate && (!toDate || currentDate <= toDate);
+
+        if (isWithinRange) {
+          // console.log("Show article:", article);
+          posts.push(article);
+          if (article.seo && article.seo.length > 0) {
+            const seoData = article.seo[0];
+            seo = {
+              title: seoData.metaTitle || "",
+              description: seoData.metaDescription || "",
+              keywords: seoData.keywords || "",
+            };
+          }
+        } else {
+          console.log("Article is not scheduled to display today.");
+        }
+      });
     } catch (error) {
-      console.error(error);
+      console.error("Error processing articles:", error);
     }
   }
 
   return (
-    <main>
-      <First
-        big={posts[0]}
-        latests={posts.slice(1, 3)}
-        blogList={posts.slice(3, posts.length)}
-      />
-    </main>
+    <>
+      <Head>
+        <title>{seo.title}</title>
+        <meta name="description" content={seo.description} />
+        <meta name="keywords" content={seo.keywords} />
+      </Head>
+      <main>
+        <First
+          big={posts[0]}
+          latests={posts.slice(1, 3)}
+          blogList={posts.slice(3, posts.length)}
+        />
+      </main>
+    </>
   );
 }
