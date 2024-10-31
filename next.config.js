2baf6dc34903f16eff99bc15ee2b75ad81a10c73
rezahashemi <reza.8139@gmail.com>
2024-10-31 18:03:22 +0330

    add api

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ module.exports = {
         protocol: "http",
         hostname: "localhost",
       },
+      {
+        protocol: "https",
+        hostname: "phpstack-171372-4936316.cloudwaysapps.com",
+      },
     ],
   },
 };
