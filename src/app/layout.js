31390f4263132370e7e68e7d99056e081d8b6225
ArmanSaffari <a.saffari256@gmail.com>
2026-03-17 06:38:15 -0400

    Add MailerLite script to RootLayout for email tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[]).push(arguments);},l=d.createElement(e),l.async=1,l.src=u,n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+ml('account', '1276449');
+            `.trim(),
+          }}
+        />
+      </head>
       <body className="min-h-dvh">
         <Suspense fallback={<div>Loading...</div>}>
           <AOSInit />
